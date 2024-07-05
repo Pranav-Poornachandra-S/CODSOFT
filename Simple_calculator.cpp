f5b7f925e677c8b505351f79237cf2b45efd7d8b
Pranav-Poornachandra-S <pranavpoorna345@gmail.com>
2024-07-05 23:09:11 +0530

    Added new arithmetic operations to the code

--- Simple_calculator.cpp
+++ Simple_calculator.cpp
@@ -1,37 +1,60 @@
 #include<iostream>
 
+double power(double x,double n)
+{
+    if(n==0)
+        return 1;
+    else    
+        return x*power(x,n-1);    
+}
 int main()
 {
-    float num1,num2;
+    double num1,num2,c;
+    int a,b;
     char ch;
-    std::cout << "Enter the first number: ";
-    std::cin >> num1;
-    std::cout << "Enter the second number: ";
-    std::cin >> num2;
-    std::cout << "Enter the operation to be performed(+,-,*,/): ";
+    std::cout << "Enter the operation to be performed(+,-,*,/,%,^): ";
     std::cin >> ch;
-    switch(ch)
+    if(ch=='%')
     {
-        case '+':
-            std::cout << "Sum= " << num1+num2 << "\n";
-            break;
-        case '-':
-            std::cout << "Difference= " << num1-num2 << "\n";
-            break; 
-        case '*': 
-            std::cout << "Product= " << num1*num2 << "\n";
-            break;
-        case '/':
-            if(num2==0)
-            {
-                std::cout << "Number cannot be divided by zero" << "\n";
+        std::cout << "Enter the first number: ";
+        std::cin >> a;
+        std::cout << "Enter the second number: ";
+        std::cin >> b;
+        std::cout << "Remainder= " << a%b << "\n";
+    }
+    else
+    {
+        std::cout << "Enter the first number: ";
+        std::cin >> num1;
+        std::cout << "Enter the second number: ";
+        std::cin >> num2;
+        switch(ch)
+        {
+            case '+':
+                std::cout << "Sum= " << num1+num2 << "\n";
                 break;
-            }
-            else
-            {
-                std::cout << "Quotient= " << num1/num2 << "\n";
+            case '-':
+                std::cout << "Difference= " << num1-num2 << "\n";
                 break;
-            }                    
+            case '*':
+                std::cout << "Product= " << num1*num2 << "\n";
+                break;
+            case '/':
+                if(num2==0)
+                {
+                    std::cout << "Number cannot be divided by zero" << "\n";
+                    break;
+                }    
+                else
+                {
+                    std::cout << "Quotient= " << num1/num2 << "\n";
+                    break;
+                }                
+            case '^':
+                c=power(num1,num2);
+                std::cout << "Power= " << c << "\n"; 
+                break;                        
+        }
     }
     return 0;
 }
